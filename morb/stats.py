a7dfdea5075a743183e0f1cc073c7c889981cc19
Philemon Brakel <pbpop3@gmail.com>
2012-01-17 16:36:30 +0100

    Made a copy of cd_stats and removed conext and mean_field

--- morb/stats.py
+++ morb/stats.py
@@ -155,3 +155,103 @@ def cd_stats(rbm, v0_vmap, visible_units, hidden_units, context_units=[], k=1, m
         
     return stats
 
+def pt_stats(rbm, v0_vmap, visible_units, hidden_units, persistent_vmap, beta, k=1):
+    """Returns stats for parallel tempering given a list of inverse temperatures beta.
+    
+    
+    """
+    # mean_field_for_gibbs is a list of units for which 'mean_field' should be used during gibbs sampling, rather than 'sample'.
+    # mean_field_for_stats is a list of units for which 'mean_field' should be used to compute statistics, rather than 'sample'.
+
+    # complete units lists
+    visible_units = rbm.complete_units_list(visible_units)
+    hidden_units = rbm.complete_units_list(hidden_units)
+    
+    # complete the supplied vmap
+    v0_vmap = rbm.complete_vmap(v0_vmap)
+    
+    h0_activation_vmap = dict((h, h.activation(v0_vmap)) for h in hidden_units)
+
+    # TODO: ask sander if this makes sense with persistent cd like algorithms.
+    h0_stats_vmap, h0_gibbs_vmap = gibbs_step(rbm, v0_vmap, hidden_units)
+            
+    exp_input = [v0_vmap[u] for u in visible_units]
+    exp_latent = [h0_gibbs_vmap[u] for u in hidden_units]
+    
+    # scan requires a function that returns theano expressions, so we cannot pass vmaps in or out. annoying.
+    def gibbs_hvh(*args):
+        h0_gibbs_vmap = dict(zip(hidden_units, args))
+        
+        v1_in_vmap = h0_gibbs_vmap.copy()
+        
+        v1_activation_vmap = dict((v, v.activation(v1_in_vmap)) for v in visible_units)
+        v1_stats_vmap, v1_gibbs_vmap = gibbs_step(rbm, v1_in_vmap, visible_units)
+
+        h1_in_vmap = v1_gibbs_vmap.copy()
+
+        h1_activation_vmap = dict((h, h.activation(h1_in_vmap)) for h in hidden_units)
+        h1_stats_vmap, h1_gibbs_vmap = gibbs_step(rbm, h1_in_vmap, hidden_units)
+            
+        # get the v1 values in a fixed order
+        v1_activation_values = [v1_activation_vmap[u] for u in visible_units]
+        v1_gibbs_values = [v1_gibbs_vmap[u] for u in visible_units]
+        v1_stats_values = [v1_stats_vmap[u] for u in visible_units]
+        
+        # same for the h1 values
+        h1_activation_values = [h1_activation_vmap[u] for u in hidden_units]
+        h1_gibbs_values = [h1_gibbs_vmap[u] for u in hidden_units]
+        h1_stats_values = [h1_stats_vmap[u] for u in hidden_units]
+        
+        return v1_activation_values + v1_stats_values + v1_gibbs_values + \
+               h1_activation_values + h1_stats_values + h1_gibbs_values
+    
+    
+    chain_start = [persistent_vmap[u] for u in hidden_units]
+    
+    
+    # The 'outputs_info' keyword argument of scan configures how the function outputs are mapped to the inputs.
+    # in this case, we want the h1_gibbs_vmap values to map onto the function arguments, so they become
+    # h0_gibbs_vmap values in the next iteration. To this end, we construct outputs_info as follows:
+    outputs_info = [None] * (len(exp_input)*3) + [None] * (len(exp_latent)*2) + list(chain_start)
+    # 'None' indicates that this output is not used in the next iteration.
+    
+    exp_output_all_list, theano_updates = theano.scan(gibbs_hvh, outputs_info = outputs_info, n_steps = k)
+    # we only need the final outcomes, not intermediary values
+    exp_output_list = [out[-1] for out in exp_output_all_list]
+            
+    # reconstruct vmaps from the exp_output_list.
+    n_input, n_latent = len(visible_units), len(hidden_units)
+    vk_activation_vmap = dict(zip(visible_units, exp_output_list[0:1*n_input]))
+    vk_stats_vmap = dict(zip(visible_units, exp_output_list[1*n_input:2*n_input]))
+    vk_gibbs_vmap = dict(zip(visible_units, exp_output_list[2*n_input:3*n_input]))
+    hk_activation_vmap = dict(zip(hidden_units, exp_output_list[3*n_input:3*n_input+1*n_latent]))
+    hk_stats_vmap = dict(zip(hidden_units, exp_output_list[3*n_input+1*n_latent:3*n_input+2*n_latent]))
+    hk_gibbs_vmap = dict(zip(hidden_units, exp_output_list[3*n_input+2*n_latent:3*n_input+3*n_latent]))
+    
+    # add the Theano updates for the persistent CD states:
+    if persistent_vmap is not None:
+        for u, v in persistent_vmap.items():
+            theano_updates[v] = hk_gibbs_vmap[u] # this should be the gibbs vmap, and not the stats vmap!
+    
+    activation_data_vmap = v0_vmap.copy() # TODO: this doesn't really make sense to have in an activation vmap!
+    activation_data_vmap.update(h0_activation_vmap)
+    activation_model_vmap = vk_activation_vmap.copy()
+    activation_model_vmap.update(hk_activation_vmap)
+    
+    stats = Stats(theano_updates) # create a new stats object
+    
+    # store the computed stats in a dictionary of vmaps.
+    stats_data_vmap = v0_vmap.copy()
+    stats_data_vmap.update(h0_stats_vmap)
+    stats_model_vmap = vk_stats_vmap.copy()
+    stats_model_vmap.update(hk_stats_vmap)
+    stats.update({
+      'data': stats_data_vmap,
+      'model': stats_model_vmap,
+    })
+            
+    stats['data_activation'] = activation_data_vmap
+    stats['model_activation'] = activation_model_vmap
+        
+    return stats
+
